491d4a98ff34f911bbd3127f249900654dc4bfe4
Lordofblueflame <68779635+Lordofblueflame@users.noreply.github.com>
2022-04-05 19:56:21 +0200

    Create GPS - Speed Control.cpp

---
+++ CodeWars/GPS Speed Control/GPS - Speed Control.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+class GpsSpeed
+{
+public:
+    static int gps(int s, std::vector<double> &x)
+    {
+      double holder = 0.0;
+      for (auto i = x.begin(); i != x.end(); ++i)
+      {
+         if(&i++ == &x.end())
+         {
+           
+           break;
+         }
+      }
+    }
+};
